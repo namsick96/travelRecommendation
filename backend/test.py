fe6ce4b338697292bf7bc180ce58ad3581034ca3
namsick96 <namsick96@naver.com>
2021-12-20 11:25:51 +0900

    flask test

---
+++ backend/test.py
@@ -0,0 +1,10 @@
+from flask import Flask,request,jsonify
+
+app = Flask(__name__)
+
+@app.route('/',methods=['POST'])
+def index():
+    content=request.json
+    return jsonify(content)
+
+app.run(host = '127.0.0.1', port=8081)
