714bb16f42518c528e848b664ac2753aa200a7fb
namsick96 <namsick96@naver.com>
2021-12-23 16:58:33 +0900

    [feat] 오탈자 수정

--- model/test.py
+++ model/test.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 def index():
     content = request.json
     model.getCourse(content)
-    data={"type":1, "first":"윗새오름", "second": "제주시청", "third": "넥슨컴퓨터박물관","restaurant1":"레스토랑 이름1","restaurant2":"레스토랑 이름2","restaurnat3":"레스토랑 이름3","alchol1":"술집 이름1","alchol2":"술집 이름2","alchol3":"술집 이름3"}
+    data={"type":1, "first":"윗새오름", "second": "제주시청", "third": "넥슨컴퓨터박물관","restaurant1":"레스토랑 이름1","restaurant2":"레스토랑 이름2","restaurant3":"레스토랑 이름3","alchol1":"술집 이름1","alchol2":"술집 이름2","alchol3":"술집 이름3"}
     jsonData=json.dumps(data,ensure_ascii=False,indent="\t")
     print(jsonData)
     
