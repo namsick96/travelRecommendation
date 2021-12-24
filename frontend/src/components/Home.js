2371fbae4c07fb87d60de6fd464131fba6b7f346
eunddodi <eundippojjack@gmail.com>
2021-12-24 21:09:21 +0900

    [fix] delete airplane

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -11,12 +11,11 @@ function Home() {
     <>
       <div className="homecont">
         <div className="titlecont">
-        <img src={airplane} className="airplane"></img>
-        <h1 className="jejutitle" >여기올레</h1>
-        <h3 className="service">당신을 위한 여행 코스 추천 서비스</h3>
-        <button className="tr button3">
-          <Link to="/teststart" style={{ textDecoration: 'none' , color: 'white',  fontFamily: 'Cafe24SsurroundAir'}}>여행 유형 테스트하고 여행 코스 추천 받기</Link>
-        </button>
+          <h1 className="jejutitle" >여기올레</h1>
+          <h3 className="service">당신을 위한 여행 코스 추천 서비스</h3>
+          <button className="tr button3">
+            <Link to="/teststart" style={{ textDecoration: 'none' , color: 'white',  fontFamily: 'Cafe24SsurroundAir'}}>여행 유형 테스트하고 여행 코스 추천 받기</Link>
+          </button>
         </div>
         <img src={jeju} className="jeju"></img>
       </div>
