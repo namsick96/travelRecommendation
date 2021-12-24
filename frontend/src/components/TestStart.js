db94a98afed40387cfad14e1d56a9d333bd7151e
Jeong Hyunji <guswl3201@naver.com>
2021-12-24 20:51:32 +0900

    [feat] home page fix, home.css fix, add font

--- frontend/src/components/TestStart.js
+++ frontend/src/components/TestStart.js
@@ -11,11 +11,11 @@ function TestStart() {
         <h4 className="bar1" style={{fontFamily : 'WandohopeR'}}>여기 올레</h4>
         <div className="testcont">
         <div className="cont">
-        <h1 className="test4">#여행 유형 테스트</h1>
-        <h3 className="question3">나는 어떤 여행가일까?</h3>
+        <h1 className="test4" style={{fontFamily: 'EliceDigitalBaeum_Bold'}}>#여행 유형 테스트</h1>
+        <h3 className="question3" style={{fontFamily: 'Cafe24SsurroundAir'}}>나는 어떤 여행가일까?</h3>
         <img className="island" src={island}></img><br></br>
         <button className="button6">
-          <Link to="/test/1" style={{ textDecoration: 'none' , color: 'white'}}>  테스트 시작하기  </Link>
+          <Link to="/test/1" style={{ textDecoration: 'none' , color: 'white', fontFamily: 'Cafe24SsurroundAir'}}>  테스트 시작하기  </Link>
         </button>
         </div>
         </div>
