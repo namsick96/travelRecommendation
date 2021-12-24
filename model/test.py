0805e2ed93c7c48b71609ab140a31c4634dafdfb
namsick96 <namsick96@naver.com>
2021-12-25 01:15:18 +0900

    [feat] merge finish

--- model/test.py
+++ model/test.py
@@ -1,10 +1,6 @@
 from flask import Flask, request, jsonify
 import model
 import json
-<<<<<<< HEAD
-=======
-
->>>>>>> tR-4
 
 app = Flask(__name__)
 
@@ -18,14 +14,6 @@ dbs = [
 @app.route("/", methods=["POST"])
 def index():
     content = request.json
-<<<<<<< HEAD
-    model.getCourse(content)
-    data={"type":1, "first":"윗새오름", "second": "제주시청", "third": "넥슨컴퓨터박물관","restaurant1":"레스토랑 이름1","restaurant2":"레스토랑 이름2","restaurant3":"레스토랑 이름3","alchol1":"술집 이름1","alchol2":"술집 이름2","alchol3":"술집 이름3"}
-    jsonData=json.dumps(data,ensure_ascii=False,indent="\t")
-    print(jsonData)
-    
-    return jsonData
-=======
 
     result_dict = model.main(content, dbs)
     result = {
@@ -42,7 +30,6 @@ def index():
     }
 
     return json.dumps(result)
->>>>>>> tR-4
 
 
 app.run(host="0.0.0.0", port=8081)
